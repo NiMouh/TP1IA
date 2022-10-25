50054c29c75b557796d22df29bf00a645d57f994
nimouh <simaoaugusto11@hotmail.com>
2022-10-25 13:42:19 +0100

    começo

---
+++ client_a_bit_smart.py
@@ -0,0 +1,456 @@
+import socket, sys
+import math
+
+interactive_flag = False
+
+depth_analysis = 1
+
+
+def pos2_to_pos1(x2):
+    return x2[0] * 8 + x2[1]
+
+
+def pos1_to_pos2(x):
+    row = x // 8
+    col = x % 8
+    return [row, col]
+
+
+# Função objetivo, recebe o estado atual e o qual jogador é que esta a ser jogado com
+# (1 para as brancas 0 para as pretas)
+def f_obj(board, play):
+    # Dá peso ás jogadas para avisar que as peças se mantenham na defensiva durante muitas jogadas
+    weight_positions = 1e-1
+    # Declaração das peças brancas e pretas em modo string
+    # a,h = torres; b,g = cavalos; c,f =bispo; d= rainha; e = rei; restantes = peões
+    w = 'abcdedghijklmnop'
+    b = 'ABCDEFGHIJKLMNOP'
+    # Declaração da valoração de cada uma das peças comidas
+    pts = [10, 7, 6, 100, 9999, 6, 7, 10, 1, 1, 1, 1, 1, 1, 1, 1]
+    # Declaração da variavel que representa a pontuação obtida pelas peças brancas
+    score_w = 0
+    # Declaração da variavel que representa a quantos movimentos foram feitos pelas peças brancas
+    score_w_positions = 0
+
+    for i, p in enumerate(w):
+        # Procura se o tabuleiro a analisar contêm essa peça
+        ex = board.find(p)
+        # Caso contenha
+        if ex >= 0:
+            # Aumenta a pontuação tendo em conta a valoração da peça dada na lista 'pts'
+            score_w += pts[i]
+            # Declaração de uma variavel que transforma a posição em 1D para 2D (x,y) no tabuleiro
+            p2 = pos1_to_pos2(ex)
+            # Aumenta a pontuação da posição dentro em conta o peso e a posição dela no eixo dos x (do lado dos brancos)
+            score_w_positions += weight_positions * p2[0]
+    # Declaração da variavel que representa a pontuação obtida pelas peças pretas
+    score_b = 0
+    # Declaração da variavel que representa a quantos movimentos foram feitos pelas peças pretas
+    score_b_positions = 0
+    for i, p in enumerate(b):
+        # Procura se o tabuleiro a analisar contêm essa peça
+        ex = board.find(p)
+        # Caso contenha
+        if ex >= 0:
+            # Aumenta a pontuação tendo em conta a valoração da peça dada na lista 'pts'
+            score_b += pts[i]
+            # Declaração de uma variavel que transforma a posição em 1D para 2D (x,y) no tabuleiro
+            p2 = pos1_to_pos2(ex)
+            # Aumenta a pontuação da posição dentro em conta o peso e a posição dela no eixo dos x (do lado dos pretos)
+            score_b_positions += weight_positions * (7 - p2[0])
+
+    # Devolve a pontuação final como a diferença (tanto do numero de peças como movimentos feitos)
+    # entre as brancas e as pretas multiplicando por a variavel 'play' para determinar se é boa para nós ou má para nós
+    return (score_w + score_w_positions - score_b - score_b_positions) * pow(-1, play)
+
+
+def find_node(tr, id):
+    if len(tr) == 0:
+        return None
+    if tr[0] == id:
+        return tr
+    for t in tr[-1]:
+        aux = find_node(t, id)
+        if aux is not None:
+            return aux
+    return None
+
+
+def get_positions_directions(state, piece, p2, directions):
+    ret = []
+    for d in directions:
+        for r in range(1, d[1] + 1):
+            if d[0] == 'N':
+                if p2[0] - r < 0:
+                    break
+                if state[pos2_to_pos1([p2[0] - r, p2[1]])] == 'z':
+                    ret.append([p2[0] - r, p2[1]])
+                    continue
+
+                if abs(ord(state[pos2_to_pos1([p2[0] - r, p2[1]])]) - ord(piece)) > 16:
+                    ret.append([p2[0] - r, p2[1]])
+                break
+
+            if d[0] == 'S':
+                if p2[0] + r > 7:
+                    break
+                if state[pos2_to_pos1([p2[0] + r, p2[1]])] == 'z':
+                    ret.append([p2[0] + r, p2[1]])
+                    continue
+
+                if abs(ord(state[pos2_to_pos1([p2[0] + r, p2[1]])]) - ord(piece)) > 16:
+                    ret.append([p2[0] + r, p2[1]])
+                break
+            if d[0] == 'W':
+                if p2[1] - r < 0:
+                    break
+                if state[pos2_to_pos1([p2[0], p2[1] - r])] == 'z':
+                    ret.append([p2[0], p2[1] - r])
+                    continue
+
+                if abs(ord(state[pos2_to_pos1([p2[0], p2[1] - r])]) - ord(piece)) > 16:
+                    ret.append([p2[0], p2[1] - r])
+                break
+            if d[0] == 'E':
+                if p2[1] + r > 7:
+                    break
+                if state[pos2_to_pos1([p2[0], p2[1] + r])] == 'z':
+                    ret.append([p2[0], p2[1] + r])
+                    continue
+
+                if abs(ord(state[pos2_to_pos1([p2[0], p2[1] + r])]) - ord(piece)) > 16:
+                    ret.append([p2[0], p2[1] + r])
+                break
+            if d[0] == 'NE':
+                if p2[0] - r < 0 or p2[1] + r > 7:
+                    break
+                if state[pos2_to_pos1([p2[0] - r, p2[1] + r])] == 'z':
+                    ret.append([p2[0] - r, p2[1] + r])
+                    continue
+
+                if abs(ord(state[pos2_to_pos1([p2[0] - r, p2[1] + r])]) - ord(piece)) > 16:
+                    ret.append([p2[0] - r, p2[1] + r])
+                break
+            if d[0] == 'SW':
+                if p2[0] + r > 7 or p2[1] - r < 0:
+                    break
+                if state[pos2_to_pos1([p2[0] + r, p2[1] - r])] == 'z':
+                    ret.append([p2[0] + r, p2[1] - r])
+                    continue
+
+                if abs(ord(state[pos2_to_pos1([p2[0] + r, p2[1] - r])]) - ord(piece)) > 16:
+                    ret.append([p2[0] + r, p2[1] - r])
+                break
+            if d[0] == 'NW':
+                if p2[0] - r < 0 or p2[1] - r < 0:
+                    break
+                if state[pos2_to_pos1([p2[0] - r, p2[1] - r])] == 'z':
+                    ret.append([p2[0] - r, p2[1] - r])
+                    continue
+
+                if abs(ord(state[pos2_to_pos1([p2[0] - r, p2[1] - r])]) - ord(piece)) > 16:
+                    ret.append([p2[0] - r, p2[1] - r])
+                break
+            if d[0] == 'SE':
+                if p2[0] + r > 7 or p2[1] + r > 7:
+                    break
+                if state[pos2_to_pos1([p2[0] + r, p2[1] + r])] == 'z':
+                    ret.append([p2[0] + r, p2[1] + r])
+                    continue
+
+                if abs(ord(state[pos2_to_pos1([p2[0] + r, p2[1] + r])]) - ord(piece)) > 16:
+                    ret.append([p2[0] + r, p2[1] + r])
+                break
+            if d[0] == 'PS':
+                if p2[0] + r > 7:
+                    break
+                if state[pos2_to_pos1([p2[0] + r, p2[1]])] == 'z':
+                    ret.append([p2[0] + r, p2[1]])
+                continue
+            if d[0] == 'PN':
+                if p2[0] - r < 0:
+                    break
+                if state[pos2_to_pos1([p2[0] - r, p2[1]])] == 'z':
+                    ret.append([p2[0] - r, p2[1]])
+                continue
+            if d[0] == 'PS2':
+                if p2[0] + r <= 7 or p2[1] + 1 <= 7:
+                    if state[pos2_to_pos1([p2[0] + r, p2[1] + 1])] != 'z':
+                        if abs(ord(state[pos2_to_pos1([p2[0] + r, p2[1] + 1])]) - ord(piece)) > 16:
+                            ret.append([p2[0] + r, p2[1] + 1])
+
+                if p2[0] + r <= 7 or p2[1] - 1 >= 0:
+                    if state[pos2_to_pos1([p2[0] + r, p2[1] - 1])] != 'z':
+                        if abs(ord(state[pos2_to_pos1([p2[0] + r, p2[1] - 1])]) - ord(piece)) > 16:
+                            ret.append([p2[0] + r, p2[1] - 1])
+                continue
+            if d[0] == 'PN2':
+                if p2[0] - r >= 0 or p2[1] + 1 <= 7:
+                    if state[pos2_to_pos1([p2[0] - r, p2[1] + 1])] != 'z':
+                        if abs(ord(state[pos2_to_pos1([p2[0] - r, p2[1] + 1])]) - ord(piece)) > 16:
+                            ret.append([p2[0] - r, p2[1] + 1])
+
+                if p2[0] - r >= 0 or p2[1] - 1 >= 0:
+                    if state[pos2_to_pos1([p2[0] - r, p2[1] - 1])] != 'z':
+                        if abs(ord(state[pos2_to_pos1([p2[0] - r, p2[1] - 1])]) - ord(piece)) > 16:
+                            ret.append([p2[0] - r, p2[1] - 1])
+                continue
+            if d[0] == 'H':
+                if p2[0] - 2 >= 0 and p2[1] - 1 >= 0:
+                    if state[pos2_to_pos1([p2[0] - 2, p2[1] - 1])] == 'z' or abs(
+                            ord(state[pos2_to_pos1([p2[0] - 2, p2[1] - 1])]) - ord(piece)) > 16:
+                        ret.append([p2[0] - 2, p2[1] - 1])
+
+                if p2[0] - 2 >= 0 and p2[1] + 1 <= 7:
+                    if state[pos2_to_pos1([p2[0] - 2, p2[1] + 1])] == 'z' or abs(
+                            ord(state[pos2_to_pos1([p2[0] - 2, p2[1] + 1])]) - ord(piece)) > 16:
+                        ret.append([p2[0] - 2, p2[1] + 1])
+
+                if p2[0] - 1 >= 0 and p2[1] + 2 <= 7:
+                    if state[pos2_to_pos1([p2[0] - 1, p2[1] + 2])] == 'z' or abs(
+                            ord(state[pos2_to_pos1([p2[0] - 1, p2[1] + 2])]) - ord(piece)) > 16:
+                        ret.append([p2[0] - 1, p2[1] + 2])
+
+                if p2[0] + 1 <= 7 and p2[1] + 2 <= 7:
+                    if state[pos2_to_pos1([p2[0] + 1, p2[1] + 2])] == 'z' or abs(
+                            ord(state[pos2_to_pos1([p2[0] + 1, p2[1] + 2])]) - ord(piece)) > 16:
+                        ret.append([p2[0] + 1, p2[1] + 2])
+
+                if p2[0] + 2 <= 7 and p2[1] + 1 <= 7:
+                    if state[pos2_to_pos1([p2[0] + 2, p2[1] + 1])] == 'z' or abs(
+                            ord(state[pos2_to_pos1([p2[0] + 2, p2[1] + 1])]) - ord(piece)) > 16:
+                        ret.append([p2[0] + 2, p2[1] + 1])
+
+                if p2[0] + 2 <= 7 and p2[1] - 1 >= 0:
+                    if state[pos2_to_pos1([p2[0] + 2, p2[1] - 1])] == 'z' or abs(
+                            ord(state[pos2_to_pos1([p2[0] + 2, p2[1] - 1])]) - ord(piece)) > 16:
+                        ret.append([p2[0] + 2, p2[1] - 1])
+
+                if p2[0] + 1 <= 7 and p2[1] - 2 >= 0:
+                    if state[pos2_to_pos1([p2[0] + 1, p2[1] - 2])] == 'z' or abs(
+                            ord(state[pos2_to_pos1([p2[0] + 1, p2[1] - 2])]) - ord(piece)) > 16:
+                        ret.append([p2[0] + 1, p2[1] - 2])
+
+                if p2[0] - 1 >= 0 and p2[1] - 2 >= 0:
+                    if state[pos2_to_pos1([p2[0] - 1, p2[1] - 2])] == 'z' or abs(
+                            ord(state[pos2_to_pos1([p2[0] - 1, p2[1] - 2])]) - ord(piece)) > 16:
+                        ret.append([p2[0] - 1, p2[1] - 2])
+    return ret
+
+
+def count_nodes(tr):
+    ret = 0
+    if len(tr) > 0:
+        for t in tr[-1]:
+            ret += count_nodes(t)
+        return (1 + ret)
+    return ret
+
+
+def get_available_positions(state, p2, piece):
+    ret = []
+    if piece in ('a', 'h', 'A', 'H'):  # Tower
+        aux = get_positions_directions(state, piece, p2, [['N', 7], ['S', 7], ['W', 7], ['E', 7]])
+        if len(aux) > 0:
+            ret.extend(aux)
+        return ret
+
+    if piece in ('c', 'f', 'C', 'F'):  # Bishop
+        aux = get_positions_directions(state, piece, p2, [['NE', 7], ['SE', 7], ['NW', 7], ['SW', 7]])
+        if len(aux) > 0:
+            ret.extend(aux)
+        return ret
+
+    if piece in ('d', 'D'):  # Queen
+        aux = get_positions_directions(state, piece, p2,
+                                       [['N', 7], ['S', 7], ['W', 7], ['E', 7], ['NE', 7], ['SE', 7], ['NW', 7],
+                                        ['SW', 7]])
+        if len(aux) > 0:
+            ret.extend(aux)
+        return ret
+
+    if piece in ('e', 'E'):  # King
+        aux = get_positions_directions(state, piece, p2,
+                                       [['N', 1], ['S', 1], ['W', 1], ['E', 1], ['NE', 1], ['SE', 1], ['NW', 1],
+                                        ['SW', 1]])
+        if len(aux) > 0:
+            ret.extend(aux)
+        return ret
+
+    if piece in ('b', 'g', 'B', 'G'):  # Horse
+        aux = get_positions_directions(state, piece, p2, [['H', 1]])
+        if len(aux) > 0:
+            ret.extend(aux)
+        return ret
+
+    # Pawn
+    if ord('i') <= ord(piece) <= ord('p'):
+        if p2[0] == 1:
+            aux = get_positions_directions(state, piece, p2, [['PS', 2]])
+            if len(aux) > 0:
+                ret.extend(aux)
+        else:
+            aux = get_positions_directions(state, piece, p2, [['PS', 1]])
+            if len(aux) > 0:
+                ret.extend(aux)
+        aux = get_positions_directions(state, piece, p2, [['PS2', 1]])
+        if len(aux) > 0:
+            ret.extend(aux)
+        return ret
+
+    if ord('I') <= ord(piece) <= ord('P'):
+        if p2[0] == 6:
+            aux = get_positions_directions(state, piece, p2, [['PN', 2]])
+            if len(aux) > 0:
+                ret.extend(aux)
+        else:
+            aux = get_positions_directions(state, piece, p2, [['PN', 1]])
+            if len(aux) > 0:
+                ret.extend(aux)
+        aux = get_positions_directions(state, piece, p2, [['PN2', 1]])
+        if len(aux) > 0:
+            ret.extend(aux)
+    return ret
+
+
+def sucessor_states(state, player):
+    ret = []
+
+    # print('Player=%d' % player)
+
+    for x in range(ord('a') - player * 32, ord('p') - player * 32 + 1):
+
+        p = state.find(chr(x))
+        if p < 0:
+            continue
+        p2 = pos1_to_pos2(p)
+
+        pos_available = get_available_positions(state, p2, chr(x))
+        # print('%c - Tot %d' % (chr(x), len(pos_available)))
+
+        for a in pos_available:
+            state_aux = list('%s' % state)
+            state_aux[p] = 'z'
+            if ord('i') <= x <= ord('p') and a[0] == 7:
+                state_aux[pos2_to_pos1(a)] = 'd'
+            elif ord('I') <= x <= ord('P') and a[0] == 0:
+                state_aux[pos2_to_pos1(a)] = 'D'
+            else:
+                state_aux[pos2_to_pos1(a)] = chr(x)
+            ret.append(''.join(state_aux))
+
+    return ret
+
+
+def insert_state_tree(tr, nv, parent):
+    nd = find_node(tr, parent[0])
+    if nd is None:
+        return None
+    nd[-1].append(nv)
+    return tr
+
+
+def expand_tree(tr, n, play):
+    if n == 0:
+        return tr
+    suc = sucessor_states(tr[0], play)
+    for s in suc:
+        tr = insert_state_tree(tr, expand_tree([s, 0, f_obj(s, play), []], n - 1, play), tr)
+    return tr
+
+
+def get_father(tr, st):
+    if len(tr) == 0:
+        return None
+    for sun in tr[-1]:
+        if sun[0] == st[0]:
+            return tr
+
+    for sun in tr[-1]:
+        aux = get_father(sun, st)
+        if aux is not None:
+            return aux
+
+    return None
+
+
+def get_next_move(tree, st):
+    fa = st
+    while fa is not None:
+        tmp = fa
+        fa = get_father(tree, fa)
+        if fa is not None:
+            st = tmp
+            # print('Father_%s_' % st[0])
+    return st
+
+
+def decide_move(board, play):
+    states = expand_tree([board, 0, f_obj(board, play), []], depth_analysis, play)
+
+    # show_tree(states, play)
+    print('Total nodes in the tree: %d' % count_nodes(states))
+
+    choice, value = minimax_alpha_beta(states, depth_analysis, play, True, -math.inf, math.inf)
+
+    next_move = get_next_move(states, choice)
+
+    return next_move[0]
+
+
+def minimax_alpha_beta(tr, d, play, max_player, alpha, beta):
+    if d == 0 or len(tr[-1]) == 0:
+        return tr, f_obj(tr[0], play)
+
+    ret = math.inf * pow(-1, max_player)
+    ret_nd = tr
+    for s in tr[-1]:
+        aux, val = minimax_alpha_beta(s, d - 1, play, not max_player, alpha, beta)
+        if max_player:
+            if val > ret:
+                ret = val
+                ret_nd = aux
+            alpha = max(alpha, ret)
+        else:
+            if val < ret:
+                ret = val
+                ret_nd = aux
+            beta = min(beta, ret)
+        if beta <= alpha:
+            break
+
+    return ret_nd, ret
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # socket initialization
+client.connect((sys.argv[1], int(sys.argv[2])))  # connecting client to server
+
+client.send(sys.argv[3].encode('ascii'))
+
+player = int(sys.argv[4])
+
+while True:  # making valid connection
+    while True:
+        message = client.recv(1024).decode('ascii')
+        if len(message) > 0:
+            break
+
+    if interactive_flag:
+        row_from = int(input('Row from > '))
+        col_from = int(input('Col from > '))
+        row_to = int(input('Row to > '))
+        col_to = int(input('Col to > '))
+
+        p_from = pos2_to_pos1([row_from, col_from])
+        p_to = pos2_to_pos1([row_to, col_to])
+
+        if (0 <= p_from <= 63) and (0 <= p_to <= 63):
+            message = list(message)
+            aux = message[p_from]
+            message[p_from] = 'z'
+            message[p_to] = aux
+            message = ''.join(message)
+    else:
+        message = decide_move(message, player)
+
+    client.send(message.encode('ascii'))
